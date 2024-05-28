ebdf339534f89f7159a4e07e80fb3539b9eacc33
cuvar <luca@cuvar.dev>
2024-05-28 18:02:26 +0200

    fix: add link icon

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -1,11 +1,11 @@
+import { Badge } from "./ui/badge";
 import {
   Card,
-  CardHeader,
   CardContent,
   CardDescription,
+  CardHeader,
   CardTitle,
 } from "./ui/card";
-import { Badge } from "./ui/badge";
 
 interface Props {
   title: string;
@@ -15,6 +15,23 @@ interface Props {
 }
 
 export function ProjectCard({ title, description, tags, link }: Props) {
+  const linkIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-3"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
+      />
+    </svg>
+  );
+
   return (
     <Card className="flex flex-col overflow-hidden border border-muted p-3">
       <CardHeader className="">
@@ -24,9 +41,10 @@ export function ProjectCard({ title, description, tags, link }: Props) {
               <a
                 href={link}
                 target="_blank"
-                className="inline-flex items-center gap-1 hover:underline"
+                className="inline-flex items-center hover:underline print:space-x-2"
               >
-                {title}{" "}
+                <span className="hidden print:block">{linkIcon}</span>
+                <span>{title} </span>
                 <span className="h-1 w-1 rounded-full bg-green-500"></span>
               </a>
             ) : (
